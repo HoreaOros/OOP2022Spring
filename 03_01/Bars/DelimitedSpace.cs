6c25c9488ebf9604b1e233081ed262a1d1ae1c3b
Alberto Tirla <albertotirla@gmail.com>
2022-03-01 13:26:19 +0200

    add a `DelimitedWhitespace` class
    in order to create a closed frame using the ascii art approach, we need a structure that encloses a line of whitespace characters of a given length in vertical bar characters, surrounding it. This is used to create the sides of the rectangle

---
+++ 03_01/Bars/DelimitedSpace.cs
@@ -0,0 +1,18 @@
+﻿namespace Bars
+{
+    class DelimitedSpace
+    {
+        public uint length { get; set; }
+        public DelimitedSpace(uint length)
+        {
+            this.length = length;
+            VerticalBar vb = new(1);
+            for (int i = 1; i <= length-2; i++)
+            {
+                Console.Write("\s");
+            }
+            vb = new(1);//pentru ca bara este desenata numai la instantierea constructorului, trebuie sa inlocuiesc variabila cu o instanta noua
+            Console.WriteLine();
+        }
+    }
+}
