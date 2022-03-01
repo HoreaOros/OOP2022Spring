a964d086a8b4601314b3a0141d6b887cceb57caa
Horea Oros <horea.oros@gmail.com>
2022-03-01 11:02:02 +0200

    add World project

---
+++ 03_01/03_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// clasa - obiect
+// tip de date - variabila de acel tip
+
+World w1 = new World();
+w1.SayHello();
+
+
+World w2 = new World();
+w2.SayHello();
+
+
+if (w1 == w2) 
+{
+    Console.WriteLine("Same worlds");
+}
+else
+{
+    Console.WriteLine("Different worlds");
+}
+
+Console.WriteLine($"id of w1: {w1.Id}");
+Console.WriteLine($"id of w2: {w2.Id}");
+
+Console.WriteLine(w1);
+Console.WriteLine(w2);
+
---
+++ 03_01/03_01/World.cs
@@ -0,0 +1,28 @@
+﻿using System;
+internal class World
+{
+    // metoda constructor implicit
+    private Guid id; 
+    public World()
+    {
+        id = Guid.NewGuid();
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello World of OOP! from {id}");
+    }
+
+    public override string ToString()
+    {
+        return string.Format("[World with id = {0}]", id);
+    }
+
+    public Guid Id
+    {
+        get
+        {
+            return id;
+        }
+    }
+}
