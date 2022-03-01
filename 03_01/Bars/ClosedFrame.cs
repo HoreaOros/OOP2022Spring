e944d3de784d385383f7a610c0b38c0e1b818bcd
Alberto Tirla <albertotirla@gmail.com>
2022-03-01 13:43:15 +0200

    add `ClosedFrame` class
    this will make it possible to draw a closed frame

---
+++ 03_01/Bars/ClosedFrame.cs
@@ -0,0 +1,19 @@
+﻿namespace Bars
+{
+    class ClosedFrame
+    {
+        public uint length { get; set; }
+        public uint height { get; set; }
+        public ClosedFrame(uint length, uint height)
+        {
+            this.length = length;
+            this.height = height;
+            HorizontalBar hb = new(length);
+            for (int i = 0; i < height-2; i++)
+            {
+                DelimitedSpace ds = new(length);
+            }
+            hb = new(length);
+        }
+    }
+}
