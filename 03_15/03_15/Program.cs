6b490c8a510b4880f6be69b84155db4962006465
Horea Oros <horea.oros@gmail.com>
2022-03-15 11:47:02 +0200

    test push method with 42 values

--- 03_15/03_15/Program.cs
+++ 03_15/03_15/Program.cs
@@ -10,3 +10,12 @@ Console.WriteLine(s.IsEmpty ? "Stiva nu contine nici un element" : "In stiva sun
 Console.WriteLine($"Capacitatea stivei este: {s.Capacity}");
 Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
 
+
+Random rnd = new Random();
+for (int i = 0; i < 42; i++)
+{
+    s.Push(rnd.Next(100));
+}
+
+Console.WriteLine($"Capacitatea stivei este: {s.Capacity}");
+Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
