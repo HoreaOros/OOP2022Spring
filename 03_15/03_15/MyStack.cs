5d8a02d83bcdcbd69f25f7e8ceb31683b88e6253
Horea Oros <horea.oros@gmail.com>
2022-03-15 11:33:26 +0200

    add Push code

--- 03_15/03_15/MyStack.cs
+++ 03_15/03_15/MyStack.cs
@@ -4,7 +4,13 @@ internal class MyStack
     private int[] data;
     private int _capacity = 16;
     private int _count = 0;
-
+    public bool IsEmpty
+    {
+        get
+        {
+            return _count == 0;
+        }
+    }
     public int Capacity 
     { 
         get 
@@ -26,6 +32,20 @@ internal class MyStack
 
     internal void Push(int val)
     {
-        
+        if (_count < _capacity)
+        {
+            data[_count] = val;
+            _count++;
+        }
+        else
+        {
+            _capacity *= 2;
+            int[] vs = new int[_capacity];
+            for (int i = 0; i < _count; i++)
+            {
+                vs[i] = data[i];
+            }
+            data = vs;
+        }
     }
 }
--- 03_15/03_15/Program.cs
+++ 03_15/03_15/Program.cs
@@ -2,6 +2,11 @@
 Console.WriteLine("Hello, World!");
 
 MyStack s = new MyStack();
+
+Console.WriteLine(s.IsEmpty ? "Stiva nu contine nici un element" : "In stiva sunt elemente");
 s.Push(1);
+Console.WriteLine(s.IsEmpty ? "Stiva nu contine nici un element" : "In stiva sunt elemente");
+
 Console.WriteLine($"Capacitatea stivei este: {s.Capacity}");
 Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
+
