c22ddad354cce4afd790fde7ad3b2a80db3caeaf
Horea Oros <horea.oros@gmail.com>
2022-03-15 11:01:23 +0200

    add stack project

---
+++ 03_15/03_15/MyStack.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class MyStack
+{
+    private int[] data;
+    private int _capacity = 16;
+    private int _count = 0;
+
+    public int Capacity 
+    { 
+        get 
+        { 
+            return _capacity; 
+        } 
+    }
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
+    }
+    public MyStack()
+    {
+        data = new int[_capacity];
+    }
+
+    internal void Push(int val)
+    {
+        
+    }
+}
---
+++ 03_15/03_15/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+MyStack s = new MyStack();
+s.Push(1);
+Console.WriteLine($"Capacitatea stivei este: {s.Capacity}");
+Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
