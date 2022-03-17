5ff21336961b31982e33fca0237e42219680e134
Horea Oros <horea.oros@gmail.com>
2022-03-17 13:03:19 +0200

    add 03_17 stck project

---
+++ 03_17/03_17/MyStack.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+internal class MyStack
+{
+    private int[] _data;
+    private int _capacity = 16;
+    private int _count = 0;
+    public MyStack()
+    {
+        _data = new int[_capacity];
+    }
+
+    public void Push(int value)
+    {
+        Debug.WriteLine($"Incerc sa adaug valoarea {value} in stiva");
+        if (Count < Capacity)
+        {
+            _data[Count] = value;
+            _count++;
+        }
+
+    }
+    public int Count 
+    { 
+        get 
+        {
+            return _count; 
+        }
+    }
+    public int Capacity
+    {
+        get
+        {
+            return _capacity;
+        }
+    }
+    
+}
---
+++ 03_17/03_17/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+MyStack s = new MyStack();
+
+Random rnd = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    s.Push(rnd.Next(100));
+}
+
+Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
+
+
