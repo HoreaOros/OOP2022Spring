ab1e5cae68f2cb5a535428043e0814c4f88eb9c2
Horea Oros <horea.oros@gmail.com>
2022-03-17 13:50:28 +0200

    add StackEmptyException class

---
+++ 03_17/03_17/StackEmptyException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+[Serializable]
+internal class StackEmptyException : Exception
+{
+    public StackEmptyException()
+    {
+    }
+
+    public StackEmptyException(string? message) : base(message)
+    {
+    }
+
+    public StackEmptyException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected StackEmptyException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
