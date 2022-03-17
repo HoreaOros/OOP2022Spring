a89a7fb6570c6f5f92d4d7b56d42f515d651d6f9
Horea Oros <horea.oros@gmail.com>
2022-03-17 13:51:24 +0200

    add Push & Pop method implementation

--- 03_17/03_17/MyStack.cs
+++ 03_17/03_17/MyStack.cs
@@ -12,13 +12,34 @@ internal class MyStack
     public void Push(int value)
     {
         Debug.WriteLine($"Incerc sa adaug valoarea {value} in stiva");
-        if (Count < Capacity)
+        if (Count == Capacity)
         {
-            _data[Count] = value;
-            _count++;
+            _capacity *= 2;
+            int[] vs = new int[_capacity];
+            for (int i = 0; i < Count; i++)
+                vs[i] = _data[i];
+            _data = vs;
+            Debug.WriteLine("Capacitatea stivei se dubleaza");
         }
-
+        _data[Count] = value;
+        _count++;
     }
+
+    public int Pop()
+    {
+
+        if (Count > 0)
+        {
+            _count--;
+            Debug.WriteLine($"Se elimina din stiva elementul {_data[_count]}");
+            return _data[_count];
+        }
+        else
+        {
+            throw new StackEmptyException("Am incercat sa elimin un element din stiva goala");
+        }
+    }
+
     public int Count 
     { 
         get 
