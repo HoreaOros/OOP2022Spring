072a3eae476d6c4e88d84a96604718ecc645de8f
Horea Oros <horea.oros@gmail.com>
2022-03-17 13:49:34 +0200

    add code to Pop stack

--- 03_17/03_17/Program.cs
+++ 03_17/03_17/Program.cs
@@ -5,11 +5,18 @@ MyStack s = new MyStack();
 
 Random rnd = new Random();
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 100; i++)
 {
     s.Push(rnd.Next(100));
 }
 
+Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
+
+int sum = 0;
+while (s.Count > 0)
+{
+    sum += s.Pop();
+}
 Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
 
 
