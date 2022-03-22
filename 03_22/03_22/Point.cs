c0e7bfafacd6eca2f2ee9af65d7e1a466a79be36
Horea Oros <horea.oros@gmail.com>
2022-03-22 10:58:47 +0200

    add Point project

---
+++ 03_22/03_22/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_22
+{
+    internal class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(): this (0, 0)
+        {
+            
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Console.WriteLine($"Am creat un obiect de tip Point ({X}, {Y})");
+        }
+
+        public Point(int x) : this(x, 0)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ 03_22/03_22/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using _03_22;
+
+Console.WriteLine("Hello, World!");
+
+Point p1 = new Point { X = 3.0, Y = 4.0 };
+Point p2 = new Point();
+Point p3 = new Point(3, 4);
+
+Console.WriteLine(p1);
+
+Point p4 = new Point(5);
+
