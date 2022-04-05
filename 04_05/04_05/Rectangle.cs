b824d5b7e58d64aa66979b6d4bfc1edd11daa5e6
Horea Oros <horea.oros@gmail.com>
2022-04-05 12:54:51 +0300

    Add Intersect method and Segment class

--- 04_05/04_05/Rectangle.cs
+++ 04_05/04_05/Rectangle.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _03_22;
+using _04_05;
 using System.Text;
-public class Rectangle : IEquatable<Rectangle>, IPolygon
+public class Rectangle : IEquatable<Rectangle>, IPolygon, ICloneable
 {
     public Point TopLeft { get; set; }
     public uint Width { get; set; }
     public uint Height { get; set; }
+
     public Rectangle(Point topLeft, uint width, uint height)
     {
         TopLeft = new Point(topLeft.X, topLeft.Y);
@@ -19,7 +21,37 @@ public class Rectangle : IEquatable<Rectangle>, IPolygon
 
     public uint ComputePerimeter() => 2 * Height + 2 * Width;
 
-    public override bool Equals(object? obj) => Equals(obj as Rectangle);
+    public override bool Equals(object? obj) => 
+        Equals(obj as Rectangle);
+    
+    public Rectangle? Intersect(Rectangle other)
+    {
+        if (Equals(other))
+            return Clone() as Rectangle;
+
+        Segment horizontal = new Segment((int)TopLeft.X, 
+            (int)TopLeft.X + (int)Width);
+        Segment horizontalOther = new Segment((int)other.TopLeft.X,
+            (int)other.TopLeft.X + (int)other.Width);
+        Segment vertical = new Segment((int)TopLeft.Y,
+            (int)TopLeft.Y + (int)Height);
+        Segment verticalOther = new Segment((int)other.TopLeft.Y,
+            (int)other.TopLeft.Y + (int)other.Height);
+
+        Segment? horizontalIntersect = horizontal
+            .Intersect(horizontalOther);
+        Segment? verticalIntersect = vertical
+            .Intersect(verticalOther);
+
+        if (horizontalIntersect == null || verticalIntersect == null)
+            return null;
+
+        return new Rectangle(new Point(horizontalIntersect.Start, 
+            verticalIntersect.Start), 
+            (uint)horizontalIntersect.Length, 
+            (uint)verticalIntersect.Length);
+    }
+    
     public override string ToString()
     {
         return new StringBuilder("Rectangle{")
@@ -52,5 +84,11 @@ public class Rectangle : IEquatable<Rectangle>, IPolygon
     public override int GetHashCode()
     {
         return HashCode.Combine(TopLeft.X, TopLeft.Y, Width, Height);
+    }
+
+    public object Clone()
+    {
+        return new Rectangle(new Point(TopLeft.X, TopLeft.Y), Width,
+            Height);
     }
 }
---
+++ 04_05/04_05/Segment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_05
+{
+    public class Segment
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public int Length => End - Start;
+
+        public Segment(int start, int end)
+        {
+            if (start > end)
+            {
+                (start, end) = (end, start);
+            }
+            Start = start;
+            End = end;
+        }
+
+        public Segment? Intersect(Segment other)
+        {
+            if (End < other.Start) return null;
+            if (Start > other.End) return null;
+
+            int maxStart = Math.Max(Start, other.Start);
+            int minEnd = Math.Min(End, other.End);
+
+            return new Segment(maxStart, minEnd);
+        }
+    }
+}
