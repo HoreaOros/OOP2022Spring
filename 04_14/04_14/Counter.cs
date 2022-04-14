34682722ae5be68437e1414b19c629ddda26dd0f
Horea Oros <horea.oros@gmail.com>
2022-04-14 12:31:58 +0300

    add 04_14 project

---
+++ 04_14/04_14/Counter.cs
@@ -0,0 +1,17 @@
+﻿namespace _04_14
+{
+    internal class Counter
+    {
+        string id;
+        int count;
+        public Counter(): this("")
+        {
+            
+        }
+        public Counter(string id)
+        {
+            this.id = id;
+            count = 0;
+        }
+    }
+}
---
+++ 04_14/04_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter();
+            c.Increment();
+            int n = 10;
+            while(n > 0)
+            {
+                c.Increment();
+                n--;
+            }
+                
+
+        }
+    }
+}
