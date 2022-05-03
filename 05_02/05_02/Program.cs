a5ac2fd57a500ef13c7318da74dbfdc3d7077c07
Horea Oros <horea.oros@gmail.com>
2022-05-03 12:50:27 +0300

    add UFWightedQuickUnion

--- 05_02/05_02/Program.cs
+++ 05_02/05_02/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _05_02;
+using System.IO;
 
+StreamReader sr = new StreamReader("largeUF.txt");
+string line;
+
+int N, p, q;
+line = sr.ReadLine();
+N = int.Parse(line);
+
+UFWeightedQuickUnion uf = new UFWeightedQuickUnion(N);
+
+while (!sr.EndOfStream)
+{
+    line = sr.ReadLine();
+    string[] tokens = line.Split(' ');
+    p = int.Parse(tokens[0]);
+    q = int.Parse(tokens[1]);
+
+    if (uf.connected(p, q))
+        continue;
+
+    uf.union(p, q);
+    //Console.WriteLine("{0} - {1}", p, q);
+}
+
+Console.WriteLine("{0} componente", uf.count());
---
+++ 05_02/05_02/UFWeghtedQuickUnion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_02
+{
+    internal class UFWeightedQuickUnion : UFQuickUnion
+    {
+        private int[] sz;
+        public UFWeightedQuickUnion(int n) : base(n)
+        {
+            sz = new int[n];
+            for (int i = 0; i < n; i++)
+                sz[i] = 1;
+        }
+        public override void union(int p, int q)
+        {
+            int pRoot = find(p);
+            int qRoot = find(q);
+
+            if (pRoot != qRoot)
+            {
+                if (sz[pRoot] < sz[qRoot])
+                {
+                    id[pRoot] = qRoot;
+                    sz[qRoot] += sz[pRoot];
+                }
+                else
+                {
+                    id[qRoot] = pRoot;
+                    sz[pRoot] += sz[qRoot];
+                }
+
+                componentNo--; // numarul de componente scade cu 1
+            }
+        }
+        
+
+    }
+}
