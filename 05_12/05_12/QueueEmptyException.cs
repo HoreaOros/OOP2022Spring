56054fe49540466d9ea859cec6bacdd086ad7201
Horea Oros <horea.oros@gmail.com>
2022-05-12 14:03:10 +0300

    add QueueEmptyException class

---
+++ 05_12/05_12/QueueEmptyException.cs
@@ -0,0 +1,22 @@
+﻿// 1.Basic queue implementation
+using System.Runtime.Serialization;
+
+[Serializable]
+internal class QueueEmptyException : Exception
+{
+    public QueueEmptyException()
+    {
+    }
+
+    public QueueEmptyException(string? message) : base(message)
+    {
+    }
+
+    public QueueEmptyException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected QueueEmptyException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
