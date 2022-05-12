24c871781ad588db53a5ceb39eafc95d26581df5
Horea Oros <horea.oros@gmail.com>
2022-05-12 14:01:47 +0300

    add Logger class

---
+++ 05_12/05_12/Logger.cs
@@ -0,0 +1,10 @@
+﻿// 1.Basic queue implementation
+public class Logger
+{
+    public static readonly Logger Instance = new Logger();
+    private Logger() { }
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
