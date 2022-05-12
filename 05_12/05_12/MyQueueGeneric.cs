7616288828c031c745ad310fdbf061db973d492e
Horea Oros <horea.oros@gmail.com>
2022-05-12 14:03:29 +0300

    add Generic Queue

---
+++ 05_12/05_12/MyQueueGeneric.cs
@@ -0,0 +1,57 @@
+﻿// 2. Basic generic queue implementation
+public class MyQueue<T>
+{
+    #region Fields
+    T[] data;
+    int capacity;
+    int head, tail;
+    int count;
+    #endregion
+    #region Ctors
+    public MyQueue() : this(42)
+    {
+    }
+    public MyQueue(int capacity)
+    {
+        this.capacity = capacity;
+        data = new T[capacity];
+        head = tail = 0;
+        count = 0;
+    }
+    #endregion
+
+    #region Properties
+    public int Count => count;
+    public bool Empty => count == 0;
+    #endregion
+
+    #region Methods
+    public void Enqueue(T val)
+    {
+        if (count < capacity)
+        {
+            if (tail >= data.Length)
+            {
+                tail = 0;
+            }
+            data[tail++] = val;
+            Logger.Instance.Log($"Am adaugat valoarea <{val}> in coada");
+            count++;
+        }
+        else
+            throw new QueueFullException("Coada este plina");
+    }
+    public T Dequeue()
+    {
+        if (count > 0)
+        {
+            T val = data[head];
+            head = (head + 1) % capacity;
+            count--;
+            return val;
+        }
+        else
+            throw new QueueEmptyException("Coada este goala");
+    }
+    #endregion
+}
--- 05_12/05_12/Program.cs
+++ 05_12/05_12/Program.cs
@@ -1,13 +1,64 @@
 ﻿// 1.Basic queue implementation
-MyQueue q1 = new MyQueue();
+/*MyQueue q1 = new MyQueue(10);
 q1.Enqueue(1);
 q1.Enqueue(2);
 q1.Enqueue(3);
 
 Console.WriteLine($"Numarul de elemente din coada {q1.Count}");
 
+int val;
 while (!q1.Empty)
 {
-    Console.WriteLine($"q1.Dequeue()");
+    val = q1.Dequeue();
+    Logger.Instance.Log($"Am scos din coada valoarea {val}");
+}
+
+*/
+
+
+/*
+int val;
+MyQueue q2 = new MyQueue(100);
+Random rnd = new Random();
+int maxValue = 1000;
+for (int i = 0; i < 100; i++)
+{
+    int op;
+    op = rnd.Next(4);
+    switch (op)
+    {
+        case 0: case 1:
+        case 2:
+            val = rnd.Next(maxValue + 1);
+            q2.Enqueue(val);
+            break;
+        case 3:
+            val = q2.Dequeue();
+            Logger.Instance.Log($"Am scos din coada valoarea {val}");
+            break;
+    }
+}
+
+*/
+
+
+MyQueue<int> q3 = new MyQueue<int>();
+q3.Enqueue(1);
+q3.Enqueue(2);
+q3.Enqueue(3);
+
+MyQueue<string> q4 = new MyQueue<string>();
+q4.Enqueue("to");
+q4.Enqueue("be");
+q4.Enqueue("or");
+q4.Enqueue("not");
+q4.Enqueue("to");
+q4.Enqueue("be");
+
+string val;
+while (!q4.Empty)
+{
+    val = q4.Dequeue();
+    Logger.Instance.Log($"Am scos din coada: <{val}>");
 }
 
